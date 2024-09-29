0b086d22cef7ba6c2a27c8ccd52138896cd081d7
JandroCode <alex.m@galsoft.es>
2024-09-29 19:20:52 +0200

    Controladores y vistas

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/uno")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/dos")
+    public String homeDos(){
+        return "homeDos";
+    }
+
+    @GetMapping("/tres")
+    public String homeTres(){
+        return "homeTres";
+    }
+}
